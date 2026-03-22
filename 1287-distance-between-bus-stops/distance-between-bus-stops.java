9991e08a24d49dc1a4fac4383958e5a7daec524b
devika-0410 <devikabhambishte@gmail.com>
2026-03-22 18:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (32.75%) - LeetSync

---
+++ 1287-distance-between-bus-stops/distance-between-bus-stops.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        int sum1 = 0, sum2 = 0;
+        if(start > destination){
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+        for(int i = start; i < destination; i++){
+            sum1 += distance[i];
+        }
+        for(int i = destination; i != start; i = (i + 1) % distance.length){
+            sum2 += distance[i];
+        }
+        return Math.min(sum1, sum2);
+    }
+}
