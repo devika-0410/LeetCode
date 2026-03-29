c41c2b24ad3a1016bb6c7bba4d85e601258b1925
devika-0410 <devikabhambishte@gmail.com>
2026-03-29 12:02:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (87.95%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,9 +1,38 @@
 class Solution {
     public int search(int[] nums, int target) {
         // Linear Search
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                return i;
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(nums[i] == target){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){   
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            // Left half sorted
+            if(nums[left] <= nums[mid]){
+                if(target >= nums[left] && target < nums[mid]){
+                    right = mid - 1;
+                } else{
+                    left = mid + 1;
+                }
+            }
+            // Right half sorted
+            else{
+                if(target > nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                } else{
+                    right = mid - 1;
+                }
             }
         }
         return -1;
