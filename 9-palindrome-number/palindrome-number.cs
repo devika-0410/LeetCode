89879a801941368a54d5a3e7e9fbf3c51191fbe5
devika-0410 <devikabhambishte@gmail.com>
2026-07-12 22:18:29 +0530

    Time: 1 ms (99.98%) | Memory: 36.9 MB (74.87%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+
+        int halfRev = 0;
+
+        while(halfRev < x){
+            halfRev = halfRev * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return halfRev == x || halfRev / 10 == x;
+    }
+}
